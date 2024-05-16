587da6e7e37e791eaa97250650557fb86d1adecb
Infernalitiy <168316334+Infernalitiy@users.noreply.github.com>
2024-05-16 09:26:03 -0400

    Update main.js

--- new-tab/main.js
+++ new-tab/main.js
@@ -9,7 +9,7 @@ const WIFI_URL = "chrome://network/#select";
 const BLUETOOTH_URL = "chrome://bluetooth-pairing";
 const SETTINGS_URL = "chrome://settings";
 const NEW_TAB_URL = "chrome://new-tab-page";
-const FILES_URL = "chrome://file-manager";
+const OS_SETTINGS = "chrome://os-settings";
 const HELP_URL = "https://github.com/bypassiwastaken/skiovox-helper";
 const WEBSTORE_URL = "https://chromewebstore.google.com";
 const ADDSESSION_URL = "https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAlAmgQ&flowName=GlifWebSignIn&flowEntry=AddSession";
@@ -25,7 +25,7 @@ let [
     colorChange,
     wifi,
     bluetooth,
-    files,
+    os_settings,
     settings
 ] = document.querySelectorAll('svg')
 
@@ -53,9 +53,9 @@ theme.addEventListener('click', () => {
     chrome.tabs.create({ url: NEW_TAB_URL })
 })
 
-files.addEventListener('click', () => {
+os_settings.addEventListener('click', () => {
     chrome.tabs.create({}, (tab) => {
-        chrome.tabs.update(tab.id, { url: FILES_URL })
+        chrome.tabs.update(tab.id, { url: OS_SETTINGS_URL })
     })
 })
 
